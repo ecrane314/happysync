a07e25cfaf923e40c33cb90ae1dce8d8924cd7ac
Evan Crane <ecrane314@gmail.com>
2018-12-09 18:42:35 -0600

    cleaned up video_push.py

--- README.md
+++ README.md
@@ -2,6 +2,12 @@ happysync is a CLI utility to perform authenticated syncronization between local
 
 It was written to sync large project files between multiple computers and additionally offer cloud backup.
 
-[WIP]
-7/4/18 Removing authentication and user directory artifacts from source
+
+7/4/18
+Removing authentication and user directory artifacts from source
 Pursuing 12 Factor... ask, "could I safely push this to public repo as-is?"
+
+12/9/2018
+Consolidating scripts and python
+Video/Picture and file archiving will be two functions in this repo
+Setting bucket to version will protect from deletion and limited modification means costs will stay low
--- video_push.py
+++ video_push.py
@@ -1,35 +1,46 @@
 #!/usr/bin/python
 
-from google.cloud import storage
 from sys import argv
 
-def videoPush(target="evancrane", dir=0):
-	client = storage.Client()
-	bucket = client.get_bucket(target)
+import google.cloud
+from google.cloud import storage
 
-	iter = bucket.list_blobs()
-	for i in bucket.list_blobs(prefix=dir):
-		print (i)
-	print("target: " + target)
-	print("prefix: " + dir)
+BUCKET_NAME = argv[1]
+
+def video_push(target=BUCKET_NAME, dir=0):
+    client = storage.Client()
+    bucket = client.get_bucket(target)
+    bucket = client.get_bucket(argv[2])
+
+    iter = bucket.list_blobs()
+    for i in bucket.list_blobs(prefix=dir):
+        print(i)
+        print("target: " + target)
+        print("prefix: " + dir)
+
+    for i in iter:
+        print (i)
+
+    print("Got to the end")
+
+    #TODO: Determine correct assignment of bucket, target or argv
+    #TODO: need to authenticate with key, not sdk, it's in $1 from shell
+    #TODO: Need the python equivalent now that I have a bucket object.
+    #gsutil -m rsync argv[3] gs://argv[2]/argv[3]
 
 
-def videoPush():
-        #TODO: need to authenticate with key, not sdk, it's in $1 from shell
-	client = storage.Client()
-    bucket = client.getBucket(argv[2)
+def iter_blobs(client):
+    try:
+        bucket = client.get_bucket(BUCKET_NAME)
+    except google.cloud.exceptions.NotFound:
+        print('Sorry, that bucket does not exist!')
 
-        #TODO: This is for command line, I need the python equivalent 
-        #now that I have a bucket object.
-        gsutil -m rsync argv[3] gs://argv[2]/argv[3]
+    blob_list = bucket.list_blobs()
+    print(blob_list)
+    print(blob_list)
 
-	for i in bucket:
-		print (i)
-	print("Got to the end")
-
-if __name__=="__main__":
-	if len(argv) == 3:
-		videoPush(argv[1], argv[2])
-	elif len(argv) == 2:
-		videoPush(argv[1])
-
+if __name__ == "__main__":
+    if len(argv) == 3:
+        video_push(argv[1], argv[2])
+    elif len(argv) == 2:
+        video_push(argv[1])
