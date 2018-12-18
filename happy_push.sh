de2bde738d22740f22802df709fd27c036e6435d
Evan Crane <ecrane314@gmail.com>
2018-12-17 21:19:40 -0600

    implemented conditionals and parameter checking

--- happy_push.sh
+++ happy_push.sh
@@ -1,53 +1,43 @@
 #!/usr/bin/env bash
 # use "env bash" instead of "bash" to pick from environment
 
-#Run the sync with the following arguments
-# $1 json api key for bucket in your project
-# $2 destinaton bucket
-# $3 local source directory
+# 2018 May 16 GCP backup script
+# Need gcloud sdk and in particular, gsutil to run this
+# .boto file in HOME can set encryption/decryption key
 
-#Turn on virtualenv inside folder created from git clone
-source env/bin/activate
+# $1 path to local directory
+SRC_PATH=$1
 
-#set credentials from argument
-export GOOGLE_APPLICATION_CREDENTIALS=$1
+# $2 gcp bucket name
+DEST_BUCKET=$2
 
-#ensure gcs module is installed
-pip install -q --upgrade google-cloud-storage
+# $3 destination path
+DEST_PATH=$3
 
-python pushToGCS.py $1 $2 $3
-
-
-
-python ./videoPush.py $1
+# $4 Dry run? Or "LIVE"?
+DRY_RUN=$4
 
 
-rsync -v -d -r -t <source> <destination>
+if [ -z "$1" -o -z "$2" -o -z "$3" ]
+    then 
+        printf "Error: Parameters Missing. Please use these three\n"
+        printf "<Source Path> <Destination Bucket> <Destination Directory>\n"
+    else
+        if [ "$DRY_RUN" == "LIVE" ]
+            then
+                printf "====LIVE SYNC====\n"
+                #gsutil -m rsync -n -C -d -e -r $SRC_PATH gs://${DEST_BUCKET}/${DEST_PATH}/
+            else
+                printf "====DRY RUN====\n"
+                #gsutil -m rsync -n -C -d -e -r $SRC_PATH gs://${DEST_BUCKET}/${DEST_PATH}/
+        fi
+        printf "Job Complete! \n"
+fi
 
-# -v verbose to show file by file
-# -d delete things in destination not present in source
-# -r recurse through folders
-# -t preserve modified times. otherwise will recopy everything each time
+# gsutil help options, # -m multithread 
+# gsutil help <command> eg rsync
+# -C continue if error
+# -d delete files in destination not present in source
+# -e skip over sym links
 # -n [not used] perform a dry run, simulating the changes and displaying results
-
-#!/bin/bash
-
-#2018 May 16 GCP backup script
-#Need gcloud sdk and in particular, gsutil to run this
-#.boto file in HOME can set encryption/decryption key
-#Music and Photos will take a long time, have those separate for now
-
-
-# -m multithread -C continue if error -d delete at destination -n dry run
-# -r copy recursively into directories
-# -v no verbose option for rsync
-
-BUCKET=$1
-dir=(develop Documents Desktop Pictures)
-
-
-gsutil -m rsync -C -d -e -r $HOME/${dir[0]} gs://${BUCKET}/${dir[0]}/
-gsutil -m rsync -C -d -e -r $HOME/${dir[1]} gs://${BUCKET}/${dir[1]}/
-gsutil -m rsync -C -d -e -r $HOME/${dir[2]} gs://${BUCKET}/${dir[2]}/
-gsutil -m rsync -C -d -e -r $HOME/${dir[3]} gs://${BUCKET}/${dir[3]}/
-echo "Job Success\n"
+# -r recurse through folders
---
+++ test_vars.sh
@@ -0,0 +1,3 @@
+if [ -z "$1" -o -z "$2" ] 
+    then printf "vars missing\n" 
+fi
