541faf09c6bddd211e28158ad7653c6ae61a9174
Evan Crane <ecrane314@gmail.com>
2022-07-10 09:05:00 -0500

    Add exclude for ds_store as has modification time

--- rsync-across-drives.sh
+++ rsync-across-drives.sh
@@ -5,12 +5,13 @@
 
 #  Skipping short hand for human readability given sensitivity
 #  Mac and gsutil rsync have a few important flag differences,eg  -d, -e
+
 # -n --dry-run  perform a dry run, simulating the changes and displaying results
 # -v --verbose   verbose
 # --delete  delete extraneous files from dest dirs
+# --exclude ".DS_Store"
 
 # --archive  same as -rlptgoD  (no -H)
-
 # -r --recursive recurse through folders
 # --links  copy symlinks as symlinks
 # -p --perms  preserve permissions
@@ -19,4 +20,5 @@
 # -o --owner preserve owner
 # -D --devices preserve device files
 
-rsync --verbose --dry-run --delete --archive <src> <dst>
+rsync --dry-run --verbose --archive --delete --exclude ".DS_Store" <src> <dst>
+rsync -nv --archive --delete --exclude ".DS_Store" <src> <dst>
