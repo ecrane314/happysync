b754ac32c65b6217b527a44d03dd397687fd3016
Evan Crane <ecrane314@gmail.com>
2023-09-30 17:13:59 -0700

    adding comments and cleanup

--- push_to_cloud.sh
+++ push_to_cloud.sh
@@ -7,13 +7,17 @@
 
 ##### TODO  Confirm when selecting LIVE
 
-# gsutil help options, # -m multithread 
+# gsutil help options, 
 # gsutil help <command> eg rsync
+# -m multithread 
 # -C continue if error
 # -d delete files in destination not present in source
 # -e skip over sym links
 # -n [not used] perform a dry run, simulating the changes and displaying results
 # -r recurse through folders
+
+# Bash script options
+# -z 
 
 # $1 path to local directory
 SRC_PATH=$1
@@ -27,7 +31,8 @@ DEST_PATH=$3
 # $4 Dry run? Or "LIVE"?
 DRY_RUN=$4
 
-
+#The -o operator is the logical OR operator. It returns true if either of its operands is true, and false only if both of its operands are false.
+#The -z operator is the string length operator. It returns true if its operand is an empty string, and false otherwise.
 if [ -z "$1" -o -z "$2" -o -z "$3" ]
     then 
         printf "Error: Parameters Missing. Please use these three\n"
